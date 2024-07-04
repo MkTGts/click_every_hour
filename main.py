71529b816404471093328387d30dcb1762515045
MkTGts <MkTGts>
2024-07-04 22:56:59 +0300

    промежуточный коммит, просто что бы сохранить. Переведено все на функции.

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from datetime import datetime
+import os
+import smtplib
 
 
-
-while True:
+def clicker():  # собирае тотметки через нажатие на энтер и записывает в файл определнного дня
     s = input("Нажмите Enter")
-    time_now = datetime.now()
-    time_string = time_now.strftime("%d.%m.%y  %H:%M:%S")
-    file_name = f'data/file_{time_now.strftime("%d_%m_%y")}.txt'
-    with open(file_name, 'a', encoding='utf-8') as file:
-        file.write(time_string + '\n')
-    print(f"Все OK, следующее нажатие должно быть в {f'{time_now.hour + 1}:{time_now.minute}'}")
+    time_now_to_datetime = datetime.now()  # время сейчас
+    time_to_wirte = time_now_to_datetime.strftime("%d.%m.%y  %H:%M:%S")
+    file_name = f"data/file_{time_now_to_datetime.strftime('%d_%m_%y')}.txt"
+    with open(file_name, 'a', encoding="utf-8") as file:
+        file.write(time_to_wirte + '\n')
+    os.system("clear")
+    print(f"Все Ok. Следующее нажатие ожидается в {time_now_to_datetime.hour}:{time_now_to_datetime.minute}\n")
+ 
+
+def mail():
+    smtpObj = smtplib.SMTP("smtp.rambler.ru", 587)  # подкл к серверку smtp
+    smtpObj.starttls()  # шифровагие TLS
+    smtpObj.login("mktgts@rambler.ru", "MrSpock14")  # авторизация на smtp server
+    smtpObj.sendmail("maksimkor96@yandex.ru", "Hello!")  # адреса почты на которые будем отправлять
+
+
+
+
+def main():
+    while True:
+        try:
+            clicker()
+        except Exception as err:
+            print(f"При работе возникла ошибка {err}. Попробуйте еще раз или обратитесь к администартору.")
+
+
+if __name__ == '__main__':
+    mail()
+
